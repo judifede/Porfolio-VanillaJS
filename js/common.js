cf767f0493ee6faa249e93701018b8bfcf5e43a0
Juan Diego Fernández Déniz <diego_ss2@hotmail.com>
2017-06-01 21:06:29 +0100

    NavBar totalmente responsive

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 function findPosition(obj) {
     var navPosition = 37;
-	if(screen.width>=1000){
+	if(window.innerWidth>=1000){
 		navPosition = 0;
 	}
     var currentTopPosition = 0 - navPosition;
@@ -25,7 +25,35 @@ function scrollToContent(content){
 			window.scroll(0,findPosition(document.getElementById(content)));
 			break;
 		default:
-
 			window.location.reload(true);
 	}
 }
+
+function styleNavResponsive(){
+
+	if(window.innerWidth<=700){ //Mobile
+		
+		for (var i =0; i < document.querySelectorAll(".material-icons").length; i++) {
+        	document.querySelectorAll(".material-icons")[i].classList.add("c-white");
+
+    	}
+		for (var i =0; i < document.querySelectorAll(".input-group-addon").length; i++) {
+        	document.querySelectorAll(".input-group-addon")[i].classList.add("bg-black");
+
+    	}
+	}
+	if(window.innerWidth>700){ //Tablet
+		
+		for (var i =0; i < document.querySelectorAll(".material-icons").length; i++) {
+        	document.querySelectorAll(".material-icons")[i].classList.remove("c-white");
+
+    	}
+		for (var i =0; i < document.querySelectorAll(".input-group-addon").length; i++) {
+        	document.querySelectorAll(".input-group-addon")[i].classList.remove("bg-black");
+
+    	}
+	}
+}
+
+var mql = window.matchMedia("screen and (max-width: 700px)");
+mql.addListener(styleNavResponsive);
