a0e78c6b231f468191b36788d21e5ac2d847f810
Juan Diego Fernández Déniz <diego_ss2@hotmail.com>
2017-08-02 13:09:04 +0100

    Imágenes editadas con Photoshop + Flecha Subir JS funcionando en escritorio normal

--- js/common.js
+++ js/common.js
@@ -6,7 +6,9 @@ function estadoScroll(){
 	var currentScroll = window.pageYOffset || document.body.scrollTop;
 	var flecha="";
 	var obj_footer = document.getElementById("footer");
-	var position_footer = findPosition(obj_footer);
+	var vh_pixels = viewportToPixels('100vh');
+	var position_footer = findPosition(obj_footer) - vh_pixels;
+
 	if(document.getElementById("flechaSubir")!=null){
 	flecha=document.getElementById("flechaSubir");
 		if(currentScroll==0){
@@ -14,13 +16,20 @@ function estadoScroll(){
 		}else if (currentScroll>0){
 			flecha.style.visibility="visible";
 		}
-		if (currentScroll>=2802) {
+		if (currentScroll>=position_footer) {
 			flecha.setAttribute("src", "img/icons/flecha-arriba-white.png");
 		}else{
 			flecha.setAttribute("src", "img/icons/flecha-arriba-black.png");
 		}
 	}
 
+}
+
+function viewportToPixels(value) {
+  var parts = value.match(/([0-9\.]+)(vh|vw)/)
+  var q = Number(parts[1])
+  var side = window[['innerHeight', 'innerWidth'][['vh', 'vw'].indexOf(parts[2])]]
+  return side * (q/100)
 }
 
 /* Main */
@@ -48,7 +57,6 @@ function scrollToContent(content){
 		case "pageProyectos":
 			window.scroll(0,findPosition(document.getElementById(content)));
 			alert(findPosition(document.getElementById(content)));
-
 			break;
 		case "proyecto-abaco":
 			window.scroll(0,findPosition(document.getElementById(content)));
@@ -57,8 +65,8 @@ function scrollToContent(content){
 			window.scroll(0,findPosition(document.getElementById(content)));
 			break;
 		case "footer":
-			window.scroll(0,findPosition(document.getElementById(content))-700);
-			alert(findPosition(document.getElementById(content))-1000);
+			window.scroll(0,findPosition(document.getElementById(content)));
+			alert(findPosition(document.getElementById(content)));
 			break;
 		default:
 			window.location.reload(true);
