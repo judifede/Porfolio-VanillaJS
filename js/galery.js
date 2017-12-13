cd0de7ab9dae9a859e13ab2054d845c5f048d80f
Juan Diego Fernández Déniz <diego_ss2@hotmail.com>
2017-12-13 20:11:47 +0000

    Proyecto renovado: animaciones, galería, ...

---
+++ js/galery.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+    
+    var num_images;
+    var img_galery_src
+    
+    function checkPage(){
+        var url = window.location.href;
+        var urlSplitted = url.split('?');
+        var actualPage = urlSplitted[1];
+        return actualPage;
+    }
+    
+    function createImages(project){
+        
+        for (var i = 1; i < num_images+1; i++) {
+            //Div
+            var slider = $('.slider');
+            var div = document.createElement("div");
+            
+            //Img
+            var img = document.createElement("img");
+            src_galery(project, i);
+            
+            var img_galery_alt = "img_galery" + i;
+            //var img_galery_id= "img_galery" + i;
+            img.setAttribute("src", img_galery_src);
+            img.setAttribute("alt", img_galery_alt);
+            //img.setAttribute("id", img_galery_id);
+            
+            //Añadir al HTML
+            slider.append(div);
+            div.append(img);
+        }
+        
+    }
+
+    function src_galery(project, i){
+
+        switch(project){
+            case 'project=abaco':
+                img_galery_src= "../img/galeria/Abaco/Abaco" + i + ".png";
+                break;
+            case 'project=senderismo':
+                img_galery_src= "../img/galeria/SenderismoGC/Senderismo" + i + ".png";
+                break;
+            case 'project=cm3ramblas':
+                img_galery_src= "../img/galeria/CentroMédicoTresRamblas/CM3Ramblas" + i + ".png";
+                break;
+            default:
+                alert("Proyecto no encontrado");
+                break;
+            
+        }
+    }
+    
+    function managementGalery(){
+        var actualPage = checkPage();
+        var obj_nombre_proyecto = document.getElementById('project_name');
+        var boton_back = document.getElementById('back');
+        
+        switch(actualPage){
+            case "project=abaco":
+                obj_nombre_proyecto.innerHTML = "Abaco";
+                num_images = 2;
+                createImages(actualPage);
+                break;
+            case "project=senderismo":
+                obj_nombre_proyecto.innerHTML = "SenderismoGC";
+                num_images = 7;
+                createImages(actualPage);
+                break;
+            case "project=cm3ramblas":
+                obj_nombre_proyecto.innerHTML = "Centro Médico Tres Ramblas";
+                num_images = 4;
+                createImages(actualPage);
+                break;
+            default:
+			    alert("Proyecto no encontrado");
+                boton_back.click();
+                break;
+        }
+        
+    }
+
+    managementGalery();   
+    
+    $('.slider').bxSlider({
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        autoHover: true,
+        pager: true,
+        responsive: true,
+        
+    });
+    
+});
