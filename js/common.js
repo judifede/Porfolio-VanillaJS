c6b4ffbdb8791d4ab18f4b9526422083c8fcae8f
Judifede <diego_ss2@hotmail.com>
2023-05-03 18:57:10 +0100

    Nuevo CV Mayo 2023, maquetación exp y proyectos

--- js/common.js
+++ js/common.js
@@ -6,26 +6,22 @@ $(document).ready(function(){
 
 	var textos = [];
 
-	$('.text_see_more').each(function(index) { // Iteramos a través de cada elemento con la clase "texto-a-contar"
+	$('.text_see_more').each(function(index) { 
 		$(this).attr('data-index', index);
-		var texto = $(this).html().trim().replace(/,/g, 'a1b2c3'); // Obtenemos el contenido de texto del elemento actual y eliminamos espacios en blanco adicionales
+		var texto = $(this).html().trim().replace(/,/g, 'a1b2c3'); 
 		textos.push(texto);
-		var palabras = texto.split(' '); // Dividimos el texto en un array de palabras
+		var palabras = texto.split(' '); 
 		palabras.forEach(function(elemento, indice) {
 			palabras[indice] = elemento.replace(/a1b2c3/g, ',');
 		});
 		
 		
-		var numeroPalabras = palabras.length; // Contamos el número de palabras en el array
-		//$(this).siblings('.resultado').append(numeroPalabras); // Agregamos el número de palabras al elemento hermano con la clase "resultado-contador"
-		//$(this).siblings('.resultado').append(texto.slice(0, 20)); // Agregamos el número de palabras al elemento hermano con la clase "resultado-contador"
-		$(this).siblings('.resultado').append(index); // Agregamos el número de palabras al elemento hermano con la clase "resultado-contador"
+		$(this).siblings('.resultado').append(index); 
 		var nuevoContenido = '<p>' + palabras.slice(0, 10).join(' ').replace(/a1b2c3/g, ',') + ' ...</p>';
-		$(this).html(nuevoContenido); // Obtenemos el contenido de texto del elemento actual y eliminamos espacios en blanco adicionales
+		$(this).html(nuevoContenido); 
 
 		$(this).append('<span class="seeMore" ' + 'data-index="' + index + '">Leer más »</span>');
 		$('.seeMore').click(function() {
-			// Deshacemos los cambios realizados y restauramos el contenido original del elemento
 			var thisIndex = $(this).data('index');
 			$('.text_see_more[data-index="' + thisIndex + '"]').html(textos[thisIndex].replace(/a1b2c3/g, ','));
 		  });
